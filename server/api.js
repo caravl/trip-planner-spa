a7b1f8c6e1e6e970ac908a7d76d159d6d5fd9065
Jacquelyn Wax <jacquelynwax@gmail.com>
2017-11-22 10:46:19 -0500

    working api get route

--- server/api.js
+++ server/api.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const api = express.Router();
-const models = require('../models')
+const models = require('../models');
 
 api.get('/', function(req, res, next) {
+  let everything;
+
   models.Hotel.findAll()
   .then(hotels => {
-    console.log(hotels)
+    everything = hotels;
   })
+
+  models.Restaurant.findAll()
+  .then(restaurants => {
+    everything = everything.concat(restaurants);
+  })
+
+  models.Activity.findAll()
+  .then(activities => {
+    everything = everything.concat(activities);
+    return everything;
+  })
+  .then(result => {
+    res.json(result);
+  })
+
 })
 
 
